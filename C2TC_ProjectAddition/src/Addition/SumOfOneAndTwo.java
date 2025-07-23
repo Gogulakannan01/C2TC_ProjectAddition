69967a292408f938cf9ba8e1ebd8669d1dce6aa0
HP <HP@LAPTOP-2M2VELP9>
2025-07-23 12:16:12 +0530

    updated project by rajavelan

--- C2TC_ProjectAddition/src/Addition/SumOfOneAndTwo.java
+++ C2TC_ProjectAddition/src/Addition/SumOfOneAndTwo.java
@@ -6,8 +6,9 @@ public class SumOfOneAndTwo {
 public static void main(String[]args) {
 	
 	Scanner s = new Scanner(System.in);
-	System.out.println("Enter a numbers");
+	System.out.println("Enter a number 1");
 	int num1 = s.nextInt();
+	System.out.println("Enter a number 2");
 	int num2 = s.nextInt();
 	int result = num1+num2;
 	System.out.println(result);
