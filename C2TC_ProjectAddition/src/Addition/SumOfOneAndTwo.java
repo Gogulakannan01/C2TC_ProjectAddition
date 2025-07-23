64915d2f7f18b1b666d10bd9e0a170bd01ee8ce2
acer <acer@DESKTOP-2DB6O0B>
2025-07-23 12:04:49 +0530

    updated

--- C2TC_ProjectAddition/src/Addition/SumOfOneAndTwo.java
+++ C2TC_ProjectAddition/src/Addition/SumOfOneAndTwo.java
@@ -1,9 +1,15 @@
 package Addition;
 
+import java.util.Scanner;
+
 public class SumOfOneAndTwo {
 public static void main(String[]args) {
-	int a=1,b=2;
-	int c=a+b;
-	System.out.println(c);
+	
+	Scanner s = new Scanner(System.in);
+	System.out.println("Enter a numbers");
+	int num1 = s.nextInt();
+	int num2 = s.nextInt();
+	int result = num1+num2;
+	System.out.println(result);
 }
 }
